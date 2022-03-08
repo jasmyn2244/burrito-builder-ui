67f5fd6667146d0e88b1a161c434b28a55a508c3
Jasmyn Hicks <jasmynh44@gmail.com>
2022-03-08 10:18:34 -0700

    Write API calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,22 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+    .then(response => response.json())
+}
+
+export const postOrder = (newOrder) => {
+  console.log("new order:", newOrder)
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newOrder)
+  })
+    .then(response => checkResponse(response))
+}
+
+const checkResponse = (response) => {
+  if (!response.ok) {
+    throw new Error(`${response.status}`)
+  } else {
+    return response.json()
+  }
 }
